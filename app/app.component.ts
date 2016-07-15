f82e91fea9943e7b6e9c70edcef8005b0a4e9078
Dennis Haegler <dennis.haegler@gmail.com>
2016-07-15 16:20:03 +0200

    Add list appending

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,35 @@
 import { Component } from '@angular/core';
+import { PlayerListComponent } from './player_list.component';
+
+class Player {
+  name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+}
 
 @Component({
   selector: 'my-app',
-  template: '<h1>My First Angular 2 App</h1>'
+  templateUrl: 'app/app.html',
+  directives: [PlayerListComponent]
 })
-export class AppComponent { }
+export class AppComponent {
+  title = 'Street Fighter Action!!!!';
+  playerName = '';
+  playerList: Player[] = [];
+
+  addPlayer() {
+    name = this.playerName.trim();
+    if (name) {
+      var player = new Player(name);
+      this.playerList.push(player);
+      this.playerName = '';
+    }
+  }
+
+  removePlayer(player: Player) {
+    var index = this.playerList.indexOf(player);
+    this.playerList.splice(index, 1);
+  }
+}
---
+++ app/player_list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'player-list',
+  templateUrl: 'app/player_list.html'
+})
+export class PlayerListComponent {
+  title = 'Ich bin eine Liste'
+}
